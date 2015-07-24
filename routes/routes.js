a49375b8f1e5478f8eae83b9e4c4297ea429df5a
chivagarg <chivagarg@gmail.com>
2015-07-24 14:35:54 -0700

    Chatcat standup

---
+++ routes/routes.js
@@ -0,0 +1,27 @@
+module.exports = function(express, app, passport){
+	var router = express.Router();
+	
+	router.get('/', function(req, res, next){
+		res.render('index', {title : 'Welcome to chatcat!'});
+	})
+	
+	router.get('/auth/facebook', passport.authenticate('facebook'));
+	router.get('/auth/facebook/callback', passport.authenticate('facebook',{
+		successRedirect:'/chatrooms',
+		failureRedirect:'/'
+	}))
+	router.get('/chatrooms', function(req, res, next){
+		res.render('chatrooms', {title : 'Welcome to my chatroom!', user:req.user});
+	})
+	
+	router.get('/setcolor', function(req, res, next){
+		req.session.favColor = "Red";
+		res.send('Setting fav color');
+	})
+	
+	router.get('/getcolor', function(req, res, next){
+		res.send('Fav color: ' + (req.session.favColor==undefined ? "Not found" : req.session.favColor));
+	})
+
+	app.use('/', router);
+}
